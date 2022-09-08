30614aefa5aa0ecb65648b345187c7e5f4eaf0ab
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-08 23:17:58 +0600

    Number- Method js

---
+++ 6.js
@@ -0,0 +1,58 @@
+// number;
+
+// var num = 23;
+
+// console.log(typeof(num));
+
+// string;
+
+// var num = '23';
+
+// console.log(typeof(num));
+
+// change number to stirng ;
+
+// var num = 2;
+// var num= toString(num);
+// console.log(typeof(num));
+
+
+//  parseInt ;
+
+
+// var num = "2";
+// var num= parseInt(num);
+// console.log(typeof(num));
+
+//  parsefloat ;
+
+// var num = "2.456";
+// var num= parseFloat(num);
+// console.log(typeof(num));
+
+// tofixed ;
+
+// var number = 2.456;
+
+// console.log(number.toFixed(2));
+
+// toPrecision;
+
+// var number = 2.456;
+
+// console.log(number.toPrecision(3));
+
+
+// strig to number convert 
+
+
+
+console.log(Number("12.4"));
+
+console.log(typeof(Number("12.4")));
+
+
+
+
+
+
